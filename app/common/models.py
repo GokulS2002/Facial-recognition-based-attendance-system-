954e6c743f900f8d391f0158f5b9b5e5bd89aaa8
Gokul <114903033+GokulS2002@users.noreply.github.com>
2024-12-06 12:25:26 +0530

    Delete app directory

--- app/common/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
- 
-
-class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15, unique=True)
-    s3_image_url = models.URLField(blank=True, null=True) 
-    created_at = models.DateTimeField(auto_now_add=True)
